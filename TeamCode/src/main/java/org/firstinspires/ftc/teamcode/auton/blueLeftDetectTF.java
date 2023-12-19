fac8cfc5dff7a4915c57107d70e8c0c728b64773
CaptainTNT <micahcv@icloud.com>
2023-12-19 16:08:57 -0500

    Worked on Blue side

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/blueLeftDetectTF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/blueLeftDetectTF.java
@@ -141,84 +141,125 @@ public class blueLeftDetectTF extends LinearOpMode {
         if (opModeIsActive()) {
 
             if(Left){
-                drive(-123, -0.4,1000);
+                drive(-123, 0.4, 1000);
 
-                spinLeft(900, 0.4,1500);
+                //Sets the robot to spin Left for 920 ticks at 0.4 power for 1.5 seconds
+                spinLeft(900, 0.4, 1500);
 
-                drive(-620, -0.4,1600);
+                //Sets the robot to drive for -620 ticks at 0.4 power for 1.6 seconds
+                drive(-620, 0.4, 1600);
 
-                strafeLeft(1450, 0.4,1800);
+                //Sets the robot to strafe Left for 1500 ticks at 0.4 power for 1.8 seconds
+                strafeLeft(1500, 0.4, 1800);
 
+                //Sets the robot to open left servo for 1.5 seconds
                 servoLeftOpen(1500);
 
-                drive(-840, -0.4,3100);
+                //Sets the robot to drive for -840 ticks at 0.4 power for 3.1 seconds
+                drive(-840, 0.4, 3100);
 
-                strafeLeft(300, 0.4,1800);
+                //Sets the robot to strafe Right for 600 ticks at 0.4 power for 1.8 seconds
+                strafeRight(400, 0.4, 1800);
 
-                arm(-1450, 0.5,false,2000);
+                //Sets the robot lift to arm for -1450 ticks at 0.5 power for 2.0 seconds
+                arm(-1450, 0.5, false, 2000);
 
+                //Sets the robot to open right flipper for 2 seconds
                 servoRightOpen(2000);
 
+                //Resets the encoders + lowers arm
                 Reset();
-                drive(200,-0.4,3100);
 
-                strafeRight(1700, 0.4,3000);
+                //Sets the robot to drive for 200 ticks at 0.4 power for 3.1 seconds
+                drive(200, 0.4, 3100);
 
-                drive(-500, -0.4,3100);
+                //Sets the robot to strafe Left for 1600 ticks at 0.4 power for 3.0 seconds
+                strafeLeft(1600, 0.4, 3000);
 
+                //Sets the robot to drive for -500 ticks at 0.4 power for 3.1 seconds
+                drive(-500, 0.4, 3100);
+
+                //Stops the robot
                 stop();
 
             } else if (Middle) {
-                drive(-1950, -0.4, 1000);
+                //Sets the robot to drive for -1950 ticks at 0.4 power for 3.0 seconds
+                drive(-1950, 0.4, 3000);
 
-                servoLeftOpen(2000);
+                //Sets the robot to open left flipper for 1.5 seconds
+                servoLeftOpen(1500);
 
-                drive(-300, -0.4, 1000);
+                //Sets the robot to drive for -300 ticks at 0.4 power for 1.8 seconds
+                drive(-300, 0.4, 1800);
 
+                //Sets the robot to spin Left for 920 ticks at 0.4 power for 2.0 seconds
                 spinLeft(920, 0.4, 2000);
 
+                //Sets the robot to lift arm for -1200 ticks at 0.6 power for 1.0 seconds
                 arm(-1200, 0.6, false, 1000);
 
-                drive(-1350, 0.4, 2000);
+                //Sets the robot to drive for -1200 ticks at 0.4 power for 2.0 seconds
+                drive(-1200, 0.4, 2000);
 
-                strafeRight(5450, 0.4, 4000);
+                //Sets the robot to strafe Right for 5000 ticks at 0.4 power for 1.5 seconds
+                strafeRight(5000, 0.4, 1500);
 
-                drive(-400, 0.4, 4000);
+                //Sets the robot to drive for -350 ticks at 0.4 power for 1.0 seconds
+                drive(-350, 0.4, 1000);
 
+                //Sets the robot to open right servo for 2000 ticks
                 servoRightOpen(2000);
 
-                strafeRight(1000, 0.4,2000);
+                //Resets the encoders + lowers arm
                 Reset();
 
-                drive(-500, -0.4,100);
+                //Sets the robot to strafe Left for 1100 ticks at 0.4 power for 2.0 seconds
+                strafeLeft(1100, 0.4, 2000);
 
+                //Sets the robot to drive for -500 ticks at 0.4 power for 3.1 seconds
+                drive(-500, 0.4, 3100);
+
+                //Stops the robot
                 stop();
 
             } else {
-                drive(-1150, -0.4,3000);
+                //Sets the robot to drive for -1150 ticks at 0.4 power for 3.0 seconds
+                drive(-1150, 0.4, 3000);
 
-                spinLeft(920, -0.4, 2000);
+                //Sets the robot to spin right for 920 ticks at 0.4 power for 2.0 seconds
+                spinLeft(920, 0.4, 2000);
 
-                strafeLeft(420, -0.4, 2000);
+                //Sets the robot to strafe right for 420 ticks at 0.4 power for 2.0 seconds
+                strafeLeft(420, 0.4, 2000);
 
-                drive(280, -0.4, 3000);
+                //Sets the robot to drive for 280 ticks at 0.4 power for 3.0 seconds
+                drive(280, 0.4, 3000);
 
+                //Sets the robot to open left flipper for 3 seconds
                 servoLeftOpen(3000);
 
-                drive(-271, -0.4, 2000);
+                //Sets the robot to drive for -271 ticks at 0.4 power for 2.0 seconds
+                drive(-271, 0.4, 2000);
 
-                arm(-1200, 0.6, false, 2000);
+                //sets the robot to lift arm for -1200 ticks at 0.6 power for 1.0 seconds
+                arm(-1200, 0.6, false, 1000);
 
+                //Sets the robot to drive for -1460 ticks at 0.4 power for 2.0 seconds
                 drive(-1460, 0.4, 2000);
 
+                // Sets the robot to open right flipper for 2 seconds
                 servoRightOpen(2000);
 
-                strafeRight(1000, 0.4,3000);
+                //Resets the encoders + lowers arm
                 Reset();
 
-                drive(-500, -0.4,3100);
+                //Sets the robot to strafe right for 1000 ticks at 0.4 power for 3.0 seconds
+                strafeLeft(1000, 0.4, 3000);
 
+                //Sets the robot to drive for -500 ticks at 0.4 power for 3.1 seconds
+                drive(-500, 0.4, 3100);
 
+                //Stops the robot
                 stop();
 
 
