80b79a21bb44ec5f441c8ecadc2f72cfa4b87a97
CaptainTNT <micahcv@icloud.com>
2024-01-02 15:28:03 -0500

    Added hardware map to config code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/workSpace/Micah/newAutonTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/workSpace/Micah/newAutonTest.java
@@ -4,8 +4,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Calabration.auto;
 
 @Disabled
 @Autonomous(name = "newAutonTest")
@@ -18,11 +21,26 @@ public class newAutonTest extends OpMode {
     public static DcMotor Launchmotor = null;
     public static Servo Servo = null;
     public static Servo Servo2 = null;
+    public void hardwareImports() {
+
+        leftDrive = hardwareMap.get(DcMotor.class, "front Left");
+        rightDrive = hardwareMap.get(DcMotor.class, "front Right");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "back Left");
+        backRightDrive = hardwareMap.get(DcMotor.class, "back Right");
+        Launchmotor = hardwareMap.get(DcMotor.class, "Launch Motor");
+        Servo = hardwareMap.get(com.qualcomm.robotcore.hardware.Servo.class, "servo1");
+        Servo2 = hardwareMap.get(com.qualcomm.robotcore.hardware.Servo.class, "servo2");
+
+        leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        Servo.setDirection(com.qualcomm.robotcore.hardware.Servo.Direction.REVERSE);
+    }
 
     public static ElapsedTime timer = new ElapsedTime();
     static motorStatus driveMotors;
 
     public static void driveMB(int target, double power) {
+
         driveReset();
         driveMotors = motorStatus.driving;
 
@@ -46,14 +64,16 @@ public class newAutonTest extends OpMode {
         timer.reset();
 
         if (timer.milliseconds() >= 700){
-            stopDrive();
+            //stopDrive();
             Reset();
         }
 
     }
 
     public static void strafeRightMB(int target, double power) {
+
         driveReset();
+
         driveMotors = motorStatus.driving;
 
         leftDrive.setTargetPosition(target);
@@ -85,6 +105,7 @@ public class newAutonTest extends OpMode {
     }
 
     public static void strafeLeftMB(int target, double power) {
+
         driveReset();
         driveMotors = motorStatus.driving;
 
@@ -117,6 +138,7 @@ public class newAutonTest extends OpMode {
     }
 
     public static void spinLeftMB(int target, double power) {
+
         driveReset();
         driveMotors = motorStatus.driving;
 
@@ -149,6 +171,7 @@ public class newAutonTest extends OpMode {
     }
 
     public static void spinRightMD(int target, double power) {
+
         driveReset();
         driveMotors = motorStatus.driving;
 
@@ -188,6 +211,7 @@ public class newAutonTest extends OpMode {
     }
 
     public static void Reset() {
+
         leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -222,13 +246,15 @@ public class newAutonTest extends OpMode {
         waiting
     }
 
-    public static void checkMotorBusy(){
-        if (leftDrive.isBusy() || rightDrive.isBusy() || backLeftDrive.isBusy() || backRightDrive.isBusy()){
+    public static void checkMotorBusy() {
+        if (leftDrive.isBusy() || rightDrive.isBusy() || backLeftDrive.isBusy() || backRightDrive.isBusy()) {
             driveMotors = motorStatus.driving;
         } else {
             driveMotors = motorStatus.waiting;
         }
     }
+
+
 
     @Override
     public void init() {
