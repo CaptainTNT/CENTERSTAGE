cd459e1af04652685d84a56055d61a07c05de1b7
CaptainTNT <micahcv@icloud.com>
2024-01-16 14:12:24 -0500

    Meet 3 auto tweaKS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/MainOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/MainOpMode.java
@@ -138,7 +138,7 @@ public class MainOpMode extends OpMode {
         if (gamepad2.left_bumper){
             newTarget = -100;
         } else if (gamepad2.right_bumper){
-            newTarget = -1150;
+            newTarget = -1250;
         }
 
         if (gamepad2.left_trigger > 0.3 && !changed1) {
